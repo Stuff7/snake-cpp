57f9f9f4358f09e741126b4b7aed4b2b4e1088fe
Armando <cuentafrst@gmail.com>
2021-08-22 02:34:34 -0500

    curly brace formatting

--- src/main.cpp
+++ src/main.cpp
@@ -4,42 +4,35 @@
 #define SCREEN_WIDTH 1280
 #define SCREEN_HEIGHT 720
 
-int main(int argc, char **argv)
-{
-  if (SDL_Init(SDL_INIT_VIDEO) < 0)
-  {
+int main(int argc, char **argv) {
+  if (SDL_Init(SDL_INIT_VIDEO) < 0) {
     printf("Error: SDL failed to initialize\nSDL Error: '%s'\n", SDL_GetError());
     return 1;
   }
 
   SDL_Window *window = SDL_CreateWindow("SLD test", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, 0);
-  if (!window)
-  {
+  if (!window) {
     printf("Error: Failed to open window\nSDL Error: '%s'\n", SDL_GetError());
     return 1;
   }
 
   SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
-  if (!renderer)
-  {
+  if (!renderer) {
     printf("Error: Failed to create renderer\nSDL Error: '%s'\n", SDL_GetError());
     return 1;
   }
 
   bool running = true;
-  while (running)
-  {
+  while (running) {
     SDL_Event event;
-    while (SDL_PollEvent(&event))
-    {
-      switch (event.type)
-      {
-      case SDL_QUIT:
-        running = false;
-        break;
+    while (SDL_PollEvent(&event)) {
+      switch (event.type) {
+        case SDL_QUIT:
+          running = false;
+          break;
 
-      default:
-        break;
+        default:
+          break;
       }
     }
 
